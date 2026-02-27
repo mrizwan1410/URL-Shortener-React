63aad0b424f4aa422b221ace0009732423fdde93
RIZWAN <morizwan1410@gmail.com>
2026-02-27 16:35:31 +0530

    adding LandingPage and AboutPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
+import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import LandingPage from './components/LandingPage'
+import AboutPage from './components/AboutPage'
 
 function App() {
   return (
-    <>
-          <h1 class="text-3xl font-bold underline">
-          Hello World!!
-          </h1>
-    </>
+      <>
+          <BrowserRouter>
+              <Routes>
+                  <Route path='/' element={<LandingPage />} />
+                  <Route path='/about' element={<AboutPage />} />
+              </Routes>
+          </BrowserRouter>
+      </>
   )
 }
 
