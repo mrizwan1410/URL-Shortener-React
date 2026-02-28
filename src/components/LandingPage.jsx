91a0205f4f42f26bf7fc533807e1ead8cd65ae37
RIZWAN <morizwan1410@gmail.com>
2026-02-28 15:07:13 +0530

    updating Landing Page

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import Card from './Card';
 
 const LandingPage = () => {
 
     let desc =
-        "Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms. Optimize your sharing strategy with Linklytics. Track clicks and manage your links seamlessly to enhance your online presence. Generate short, memorable links with ease using Linklytics’s intuitive interface. Share URLs effortlessly across platforms.";
+        "Generate short, memorable links with ease using ShortLinks' intuitive interface. Share URLs effortlessly across platforms. Optimize your sharing strategy with ShortLinks. Track clicks and manage your links seamlessly to enhance your online presence. Generate short, memorable links with ease using ShortLinks intuitive interface. Share URLs effortlessly across platforms.";
 
 
     return (
         <div className="min-h-[calc(100vh-64px)]  lg:px-14 sm:px-8 px-4">
             <div className="lg:flex-row flex-col    lg:py-5   pt-16   lg:gap-10 gap-8 flex justify-between items-center">
                 <div className=" flex-1">
-                    <h1 className='font-bold font-roboto text-slate-800 md:text-5xl text-3xl md:leading-[55px] sm:leading-[45px] leading-10 lg:w-full md:w-[70%] w-full'>
+                    <h1 className='font-bold font-roboto text-slate-800 md:text-5xl text-3xl md:leading-13.75 sm:leading-11.25 leading-10 lg:w-full md:w-[70%] w-full'>
                     ShortLinks Simplifies URL Shorteneing For Efficient Sharing
                     </h1>
                     <p className="text-slate-700 text-sm my-5">
@@ -21,7 +22,7 @@ const LandingPage = () => {
                     </p>
 
                     <div className="flex items-center gap-3">
-                        <button className="bg-custom-gradient w-40 text-white rounded-md py-2">
+                        <button className="border-btnColor border w-40 text-btnColor rounded-md py-2">
                             Manage Links
                         </button>
                         <button className="border-btnColor border w-40 text-btnColor rounded-md py-2">
@@ -30,9 +31,35 @@ const LandingPage = () => {
                     </div>
                 </div>
 
-                <div className="flex-1 flex jsutify-center w-full">
-                    <img className="sm:w-[480px] w-[400px] object-cover rounded-md" src="/images/img1.png" alt=""/>
+                <div className="flex-1 flex justify-center w-full">
+                    <img className="sm:w-120 w-100 object-cover rounded-md" src="/images/img1.png" alt=""/>
                 </div>
+            </div>
+
+            <div className="sm:pt-12 pt-7">
+                <p className="text-slate-800 font-roboto font-bold lg:w-[60%]  md:w-[70%] sm:w-[80%] mx-auto text-3xl text-center">
+                    Trusted by individuals and teams at the world best companies
+                </p>
+
+                <div className="pt-4 pb-7 grid lg:gap-7 gap-4 xl:grid-cols-4  lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 mt-4">
+          <Card
+            title="Simple URL Shortening"
+            desc="Experience the ease of creating short, memorable URLs in just a few clicks. Our intuitive interface and quick setup process ensure you can start shortening URLs without any hassle."
+          />
+          <Card
+            title="Powerful Analytics"
+            desc="Gain insights into your link performance with our comprehensive analytics dashboard. Track clicks, geographical data, and referral sources to optimize your marketing strategies."
+          />
+          <Card
+            title="Enhanced Security"
+            desc="Rest assured with our robust security measures. All shortened URLs are protected with advanced encryption, ensuring your data remains safe and secure."
+          />
+          <Card
+            title="Fast and Reliable"
+            desc="Enjoy lightning-fast redirects and high uptime with our reliable infrastructure. Your shortened URLs will always be available and responsive, ensuring a seamless experience for your users.
+"
+          />
+        </div>
             </div>
         </div>
     )
