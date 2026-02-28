1027d628226be0ea3170d089914f77f173e0c11b
RIZWAN <morizwan1410@gmail.com>
2026-02-28 15:42:53 +0530

    setting Footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import {BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
 
 function App() {
   return (
       <>
           <BrowserRouter>
+          <NavBar />
               <Routes>
                   <Route path='/' element={<LandingPage />} />
                   <Route path='/about' element={<AboutPage />} />
               </Routes>
+              <Footer />
           </BrowserRouter>
       </>
   )
