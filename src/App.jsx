e19d090dec2518d1dac2937240166492db1c88ee
RIZWAN <morizwan1410@gmail.com>
2026-03-01 15:01:37 +0530

    Register Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import {BrowserRouter, Route, Routes } from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import './App.css'
 import LandingPage from './components/LandingPage'
 import AboutPage from './components/AboutPage'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
+import RegisterPage from './components/RegisterPage'
 
 function App() {
   return (
       <>
-          <BrowserRouter>
+          <Router>
           <NavBar />
               <Routes>
                   <Route path='/' element={<LandingPage />} />
                   <Route path='/about' element={<AboutPage />} />
+                  <Route path='/register' element={<RegisterPage />} />
               </Routes>
               <Footer />
-          </BrowserRouter>
+          </Router>
       </>
   )
 }
